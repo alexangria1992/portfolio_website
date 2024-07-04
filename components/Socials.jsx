42a092b311f4a8c313da520dc46cad6c46d4edeb
Alexandre Angrignon <alex040892@gmail.com>
2024-07-04 13:13:20 +1200

    added socials links

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -2,17 +2,25 @@ import Link from 'next/link';
 import { FaGithub, FaLinkedinIn, FaFacebook, FaTwitter } from 'react-icons/fa';
 
 const socials = [
-  { icon: <FaGithub />, path: '' },
-  { icon: <FaLinkedinIn />, path: '' },
-  { icon: <FaFacebook />, path: '' },
-  { icon: <FaTwitter />, path: '' },
+  { icon: <FaGithub />, path: 'https://github.com/alexangria1992' },
+  {
+    icon: <FaLinkedinIn />,
+    path: 'https://www.linkedin.com/in/alexandre-angrignon-781b95176/',
+  },
+  { icon: <FaFacebook />, path: 'https://www.facebook.com/alexbenjaminangri/' },
+  { icon: <FaTwitter />, path: 'https://x.com/alex040892' },
 ];
 const Social = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
       {socials.map((item, index) => {
         return (
-          <Link key={index} href={item.path} className={iconStyles}>
+          <Link
+            key={index}
+            href={item.path}
+            className={iconStyles}
+            target='_blank'
+          >
             {item.icon}
           </Link>
         );
